55339f4d47eebc4f6571d53ed5c8961343999b9b
Jacob Seiler <seiler.jacob1@gmail.com>
2022-12-04 22:13:56 -0500

    Updated template

--- template/template.py
+++ template/template.py
@@ -1,7 +1,11 @@
 from pathlib import Path
 
 path = (Path(__file__).parent / "./template.txt").resolve()
+data = []
 
 with open(path) as f:
     for line in f:
-        print(line.strip())
+        data.append(line.strip())
+
+for line in data:
+    print(line)

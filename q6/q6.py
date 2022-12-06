bc0d3fe4b9d88459c27185573554c3bfe3dcb366
Jacob Seiler <seiler.jacob1@gmail.com>
2022-12-06 00:08:22 -0500

    Added comments

--- q6/q6.py
+++ q6/q6.py
@@ -8,6 +8,7 @@ with open(path) as f:
         line = line.strip()
         data = line
 
+# Part A
 def findSpot():
     for i in range(3, len(data)):
         seen = set([data[i - 3], data[i - 2], data[i - 1], data[i]])
@@ -18,6 +19,7 @@ def findSpot():
 
 print(findSpot())
 
+# Part B
 def findSpot2():
     for i in range(14, len(data)):
         seen = set([data[i - 13], data[i - 12], data[i - 11], data[i - 10], data[i - 9], data[i - 8], data[i - 7], data[i - 6], data[i - 5], data[i - 4], data[i - 3], data[i - 2], data[i - 1], data[i]])
